da1ef70b8fbbd1b5172055f1150c6f0df41444e3
Suryabala Singh <22298166+suryabalasingh@users.noreply.github.com>
2020-01-13 16:25:08 +0000

    PODS-3785-Refactored Spec

--- test/transformations/userAnswersToETMP/AFTReturnTransformerSpec.scala
+++ test/transformations/userAnswersToETMP/AFTReturnTransformerSpec.scala
@@ -21,7 +21,6 @@ import play.api.libs.json.Json
 import transformations.generators.AFTGenerators
 
 class AFTReturnTransformerSpec extends FreeSpec with AFTGenerators {
-
   private val chargeFTransformer = new ChargeFTransformer
   private val chargeATransformer = new ChargeATransformer
   private val chargeBTransformer = new ChargeBTransformer
@@ -29,32 +28,37 @@ class AFTReturnTransformerSpec extends FreeSpec with AFTGenerators {
   private val chargeETransformer = new ChargeETransformer
   private val chargeDTransformer = new ChargeDTransformer
 
+  "An AFTReturn Transformer" - {
+    "must transform from UserAnswers to ETMP AFT Return format" in {
+      val transformer = new AFTReturnTransformer(chargeATransformer, chargeBTransformer,
+        chargeCTransformer, chargeDTransformer, chargeETransformer, chargeFTransformer)
+      val transformedEtmpJson = userAnswersRequestJson.transform(transformer.transformToETMPFormat).asOpt.value
+      transformedEtmpJson mustBe etmpResponseJson
+    }
+  }
+
   private val userAnswersRequestJson = Json.parse(
     """{
       |  "aftStatus": "Compiled",
       |  "quarter": {
-      |       "startDate": "2019-01-01",
-      |       "endDate": "2019-03-31"
+      |    "startDate": "2019-01-01",
+      |    "endDate": "2019-03-31"
       |  },
       |  "chargeADetails": {
-      |      "numberOfMembers": 2,
-      |      "totalAmtOfTaxDueAtLowerRate": 200.02,
-      |      "totalAmtOfTaxDueAtHigherRate": 200.02,
-      |      "totalAmount": 200.02
-      |    },
-      |    "chargeBDetails": {
-      |      "numberOfDeceased": 4,
-      |      "amountTaxDue": 55.55
-      |    },
-      |  "chargeFDetails": {
-      |    "amountTaxDue": 200.02,
-      |    "deRegistrationDate": "1980-02-29"
+      |    "numberOfMembers": 2,
+      |    "totalAmtOfTaxDueAtLowerRate": 200.02,
+      |    "totalAmtOfTaxDueAtHigherRate": 200.02,
+      |    "totalAmount": 200.02
+      |  },
+      |  "chargeBDetails": {
+      |    "numberOfDeceased": 4,
+      |    "amountTaxDue": 55.55
       |  },
       |  "chargeCDetails": {
       |    "isSponsoringEmployerIndividual": true,
       |    "chargeDetails": {
       |      "paymentDate": "2020-01-01",
-      |      "amountTaxDue": "500.02"
+      |      "amountTaxDue": 500.02
       |    },
       |    "sponsoringIndividualDetails": {
       |      "firstName": "testFirst",
@@ -62,13 +66,55 @@ class AFTReturnTransformerSpec extends FreeSpec with AFTGenerators {
       |      "nino": "AB100100A"
       |    },
       |    "sponsoringEmployerAddress": {
-      |     "line1": "line1",
-      |     "line2": "line2",
-      |     "line3": "line3",
-      |     "line4": "line4",
-      |     "postcode": "NE20 0GG",
-      |     "country": "GB"
+      |      "line1": "line1",
+      |      "line2": "line2",
+      |      "line3": "line3",
+      |      "line4": "line4",
+      |      "postcode": "NE20 0GG",
+      |      "country": "GB"
       |    }
+      |  },
+      |  "chargeDDetails": {
+      |    "numberOfMembers": 2,
+      |    "members": [
+      |      {
+      |        "memberDetails": {
+      |          "firstName": "firstName",
+      |          "lastName": "lastName",
+      |          "nino": "AC100100A",
+      |          "isDeleted": false
+      |        },
+      |        "chargeDetails": {
+      |          "dateOfEvent": "2020-01-10",
+      |          "taxAt25Percent": 100,
+      |          "taxAt55Percent": 100.02
+      |        }
+      |      }
+      |    ],
+      |    "totalChargeAmount": 200.02
+      |  },
+      |  "chargeEDetails": {
+      |    "members": [
+      |      {
+      |        "memberDetails": {
+      |          "firstName": "eFirstName",
+      |          "lastName": "eLastName",
+      |          "nino": "AE100100A",
+      |          "isDeleted": false
+      |        },
+      |        "annualAllowanceYear": "2020",
+      |        "chargeDetails": {
+      |          "dateNoticeReceived": "2020-01-11",
+      |          "chargeAmount": 200.02,
+      |          "isPaymentMandatory": true
+      |        }
+      |      }
+      |    ],
+      |    "totalChargeAmount": 200.02
+      |  },
+      |  "chargeFDetails": {
+      |    "amountTaxDue": 200.02,
+      |    "deRegistrationDate": "1980-02-29"
       |  }
       |}""".stripMargin)
 
@@ -80,56 +126,81 @@ class AFTReturnTransformerSpec extends FreeSpec with AFTGenerators {
       |    "quarterEndDate": "2019-03-31"
       |  },
       |  "chargeDetails": {
-      |     "chargeTypeADetails": {
-      |         "numberOfMembers": 2,
-      |         "totalAmtOfTaxDueAtLowerRate": 200.02,
-      |         "totalAmtOfTaxDueAtHigherRate": 200.02,
-      |         "totalAmount": 200.02
-      |       },
-      |       "chargeTypeBDetails": {
+      |    "chargeTypeADetails": {
+      |      "numberOfMembers": 2,
+      |      "totalAmtOfTaxDueAtLowerRate": 200.02,
+      |      "totalAmtOfTaxDueAtHigherRate": 200.02,
+      |      "totalAmount": 200.02
+      |    },
+      |    "chargeTypeBDetails": {
       |      "numberOfMembers": 4,
       |      "totalAmount": 55.55
       |    },
-      |       "chargeTypeFDetails": {
-      |         "totalAmount": 200.02,
-      |         "dateRegiWithdrawn": "1980-02-29"
-      |       }
-      |     },
-      |     "chargeTypeCDetails": {
-      |			"totalAmount": 500.02,
-      |			"memberDetails": [{
-      |				"memberStatus": "New",
-      |				"memberTypeDetails": {
-      |					"memberType": "Individual",
-      |					"individualDetails": {
-      |						"firstName": "testFirst",
-      |						"lastName": "testLast",
-      |						"nino": "AB100100A"
-      |					}
-      |				},
-      |				"correspondenceAddressDetails": {
-      |					"nonUKAddress": "False",
-      |					"addressLine1": "line1",
-      |					"addressLine2": "line2",
-      |					"addressLine3": "line3",
-      |					"addressLine4": "line4",
-      |					"countryCode": "GB",
-      |					"postCode": "NE20 0GG"
-      |				},
-      |				"dateOfPayment": "2020-01-01",
-      |				"totalAmountOfTaxDue": 500.02
-      |			}]
-      |		}
+      |    "chargeTypeCDetails": {
+      |      "totalAmount": 500.02,
+      |      "memberDetails": [
+      |        {
+      |          "memberStatus": "New",
+      |          "memberTypeDetails": {
+      |            "memberType": "Individual",
+      |            "individualDetails": {
+      |              "firstName": "testFirst",
+      |              "lastName": "testLast",
+      |              "nino": "AB100100A"
+      |            }
+      |          },
+      |          "correspondenceAddressDetails": {
+      |            "nonUKAddress": "False",
+      |            "postCode": "NE20 0GG",
+      |            "addressLine1": "line1",
+      |            "addressLine2": "line2",
+      |            "addressLine3": "line3",
+      |            "addressLine4": "line4",
+      |            "countryCode": "GB"
+      |          },
+      |          "dateOfPayment": "2020-01-01",
+      |          "totalAmountOfTaxDue": 500.02
+      |        }
+      |      ]
+      |    },
+      |    "chargeTypeDDetails": {
+      |      "totalAmount": 200.02,
+      |      "memberDetails": [
+      |        {
+      |          "memberStatus": "New",
+      |          "individualsDetails": {
+      |            "firstName": "firstName",
+      |            "lastName": "lastName",
+      |            "nino": "AC100100A"
+      |          },
+      |          "dateOfBeneCrysEvent": "2020-01-10",
+      |          "totalAmtOfTaxDueAtLowerRate": 100,
+      |          "totalAmtOfTaxDueAtHigherRate": 100.02
+      |        }
+      |      ]
+      |    },
+      |    "chargeTypeEDetails": {
+      |      "totalAmount": 200.02,
+      |      "memberDetails": [
+      |        {
+      |          "memberStatus": "New",
+      |          "individualsDetails": {
+      |            "firstName": "eFirstName",
+      |            "lastName": "eLastName",
+      |            "nino": "AE100100A"
+      |          },
+      |          "amountOfCharge": 200.02,
+      |          "taxYearEnding": "2020",
+      |          "dateOfNotice": "2020-01-11",
+      |          "paidUnder237b": "Yes"
+      |        }
+      |      ]
+      |    },
+      |    "chargeTypeFDetails": {
+      |      "totalAmount": 200.02,
+      |      "dateRegiWithdrawn": "1980-02-29"
+      |    }
+      |  }
       |}
-      |
       |""".stripMargin)
-
-  "An AFTReturn Transformer" - {
-    "must transform from UserAnswers to ETMP AFT Return format" in {
-      val transformer = new AFTReturnTransformer(chargeATransformer, chargeBTransformer,
-        chargeCTransformer, chargeDTransformer, chargeETransformer, chargeFTransformer)
-      val transformedEtmpJson = userAnswersRequestJson.transform(transformer.transformToETMPFormat).asOpt.value
-      transformedEtmpJson mustBe etmpResponseJson
-    }
-  }
 }
