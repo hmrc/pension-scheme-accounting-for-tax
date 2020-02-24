92f80f1414dea2956c9982f021c1b5e485d59d36
Vincent Vinnicombe <->
2020-02-24 08:32:01 +0000

    library-upgrades: library upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,6 +1,4 @@
 import play.core.PlayVersion.current
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
@@ -8,11 +6,11 @@ object AppDependencies {
   val compile = Seq(
 
     "uk.gov.hmrc"             %% "simple-reactivemongo"     % "7.23.0-play-26",
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.3.0"
+    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.4.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.3.0" % Test classifier "tests",
+    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.4.0" % Test classifier "tests",
     "org.scalatest"           %% "scalatest"                % "3.0.8"                 % "test",
     "com.typesafe.play"       %% "play-cache"               % current                 % "test",
     "org.mockito"             % "mockito-all"               % "1.10.19"               % "test",
