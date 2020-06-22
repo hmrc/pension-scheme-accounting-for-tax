e66ae3bcaf387d0c45fb1b0a3f78e6cb58f9bef7
Suryabala Singh <22298166+suryabalasingh@users.noreply.github.com>
2020-06-22 11:15:59 +0100

    PODS-4401-Fixed tests

--- app/controllers/EmailResponseController.scala
+++ app/controllers/EmailResponseController.scala
@@ -68,8 +68,6 @@ class EmailResponseController @Inject()(
       "\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-zA-Z0-9-]*[a-zA-Z0-9]:" +
       "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])$"
 
-    println("\n\n\n psaId: "+psaId)
-    println("\n\n\n emailAddress: "+emailAddress)
     try {
       require(emailAddress.matches(emailRegex))
       Right(Tuple2(PsaId(psaId), emailAddress))
