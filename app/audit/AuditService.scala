a7e56380f22d26bd4be07df8242f6adf070e63fe
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2021-01-18 13:44:19 +0000

    PODS-4981 WIP

--- app/audit/AuditService.scala
+++ app/audit/AuditService.scala
@@ -51,6 +51,7 @@ class AuditServiceImpl @Inject()(
                                            ec: ExecutionContext): Unit = {
 
     Logger.debug(s"[AuditService][sendEvent] sending ${event.auditType}")
+
     val result: Future[AuditResult] = connector.sendExtendedEvent(
       ExtendedDataEvent(
         auditSource = config.appName,
