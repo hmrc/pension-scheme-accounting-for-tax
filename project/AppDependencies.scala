ff58c1ec99a9aa0f2f1ebb7ea27cc209dc3f2b17
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2021-01-21 12:33:46 +0000

    PODS-5027: Sbt upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,11 +11,11 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.16.0" % Test classifier "tests",
+    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.16.0"                % Test classifier "tests",
     "org.scalatest"           %% "scalatest"                % "3.0.8"                 % "test",
     "com.typesafe.play"       %% "play-cache"               % current                 % "test",
-    "org.mockito"             % "mockito-all"               % "1.10.19"               % "test",
-    "com.github.tomakehurst"  % "wiremock"                  % "2.26.0"                % "test",
+    "org.mockito"             %  "mockito-all"              % "1.10.19"               % "test",
+    "com.github.tomakehurst"  %  "wiremock"                 % "2.26.0"                % "test",
     "org.scalacheck"          %% "scalacheck"               % "1.14.0"                % "test",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.2"                 % "test, it",
