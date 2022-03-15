d2a30c8f68ccfdf1dd40962f2fded1cd094deefe
grant.robson <grant.robson@digital.hmrc.gov.uk>
2022-03-15 15:48:37 +0000

    PODS-6600 WIP

--- app/controllers/cache/FinancialInfoCreditAccessController.scala
+++ app/controllers/cache/FinancialInfoCreditAccessController.scala
@@ -18,13 +18,11 @@ package controllers.cache
 
 import com.google.inject.Inject
 import play.api.Logger
-import play.api.mvc.Results.Redirect
+import play.api.libs.json.Json
 import play.api.mvc._
 import repository.FinancialInfoCreditAccessRepository
 import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals
-import uk.gov.hmrc.auth.core.retrieve.~
-import uk.gov.hmrc.auth.core.{AuthConnector, AuthorisedFunctions, Enrolment}
-import uk.gov.hmrc.domain.{PsaId, PspId}
+import uk.gov.hmrc.auth.core.{AuthConnector, AuthorisedFunctions}
 import uk.gov.hmrc.http.{HeaderCarrier, UnauthorizedException}
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
@@ -41,7 +39,7 @@ class FinancialInfoCreditAccessController @Inject()(
 
   private val logger = Logger(classOf[FinancialInfoCreditAccessController])
 
-  def schemeAccess(pstr:String): Action[AnyContent] = Action.async {
+  def schemeAccess(srn: String): Action[AnyContent] = Action.async {
     /*
     Check if a PSA/PSP accessed scheme in last 28 days and if so return Ok and:-
     PsaID: Option[String]
@@ -51,30 +49,37 @@ class FinancialInfoCreditAccessController @Inject()(
      */
     implicit request =>
       withId { (optPsaId, optPspId) =>
-        repository.get(id).map { response =>
-          logger.debug(message = s"FinancialInfoCreditAccessController.get: Response for request Id $id is $response")
-          response.map {
-            Ok(_)
-          } getOrElse NotFound
+        repository.get(srn).map { response =>
+          logger.debug(message = s"FinancialInfoCreditAccessController.get: Response for request Id $srn is $response")
+          response match {
+            case Some(jsValue) => Ok(jsValue)
+            case _ =>
+              val jsObject =
+                optPsaId.map(psaId => Json.obj("psaId" -> psaId)).getOrElse(Json.obj()) ++
+                  optPspId.map(pspId => Json.obj("pspId" -> pspId)).getOrElse(Json.obj())
+              repository.save(
+                srn,
+                jsObject
+              )
+              NotFound
+          }
         }
       }
   }
 
   private def withId(block: (Option[String], Option[String]) => Future[Result])
-                   (implicit hc: HeaderCarrier): Future[Result] = {
+                    (implicit hc: HeaderCarrier): Future[Result] = {
     authorised().retrieve(Retrievals.allEnrolments) { enrolments =>
-        val psaId = enrolments.getEnrolment("HMRC-PODS-ORG").flatMap(_.getIdentifier("PSAID")).map(_.value)
-        val pspId = enrolments.getEnrolment("HMRC-PODSPP-ORG").flatMap(_.getIdentifier("PSPID")).map(_.value)
-        block.apply(psaId, pspId)
+      val psaId = enrolments.getEnrolment("HMRC-PODS-ORG").flatMap(_.getIdentifier("PSAID")).map(_.value)
+      val pspId = enrolments.getEnrolment("HMRC-PODSPP-ORG").flatMap(_.getIdentifier("PSPID")).map(_.value)
+      (psaId, pspId) match {
+        case (None, None) => throw IdNotFoundFromAuth()
+        case _ => block.apply(psaId, pspId)
+      }
     }
   }
 }
 
 object FinancialInfoCreditAccessController {
-
   case class IdNotFoundFromAuth() extends UnauthorizedException("Not Authorised - Unable to retrieve id")
-
 }
-
-
-
