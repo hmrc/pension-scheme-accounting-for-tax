1795bee6a1b2906d2ad8f493ba33ddb3ba7634c8
emrawsy <27211889+emrawsy@users.noreply.github.com>
2025-04-09 10:12:58 +0100

    DDCNL-10351 bump dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,17 +10,17 @@ object AppDependencies {
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"         % bootstrapVersion,
     "uk.gov.hmrc"                   %% "domain-play-30"                    % "9.0.0",
     "com.github.java-json-tools"    %% "json-schema-validator"             % "2.2.14",
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"              % "2.16.1"
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"              % "2.18.3"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %%  "bootstrap-test-play-30"      % bootstrapVersion    % Test,
     "uk.gov.hmrc.mongo"       %%  "hmrc-mongo-test-play-30"     % mongoVersion    % Test,
-    "com.vladsch.flexmark"    %   "flexmark-all"                % "0.64.6"   % Test,
-    "org.scalatest"           %%  "scalatest"                   % "3.2.15"   % Test,
-    "org.scalatestplus.play"  %%  "scalatestplus-play"          % "5.1.0"    % Test,
+    "com.vladsch.flexmark"    %   "flexmark-all"                % "0.64.8"   % Test,
+    "org.scalatest"           %%  "scalatest"                   % "3.2.19"   % Test,
+    "org.scalatestplus.play"  %%  "scalatestplus-play"          % "7.0.1"    % Test,
     "org.scalatestplus"       %%  "mockito-4-6"                 % "3.2.15.0" % Test,
-    "org.scalatestplus"       %%  "scalacheck-1-17"             % "3.2.15.0" % Test,
+    "org.scalatestplus"       %%  "scalacheck-1-17"             % "3.2.18.0" % Test,
     "org.pegdown"             %   "pegdown"                     % "1.6.0"    % Test
   )
 }
