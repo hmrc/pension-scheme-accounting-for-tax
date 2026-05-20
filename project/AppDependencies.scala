fc3528cba2642b266cd4b4fe0e21afd1edbb4d34
jmsatheesh <19515164+jmsatheesh@users.noreply.github.com>
2026-05-20 16:00:59 +0100

    dependency upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
   private val mongoVersion = "2.12.0"
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"        % mongoVersion,
     "uk.gov.hmrc"                %% "bootstrap-backend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"                %% "domain-play-30"            % "12.1.0",
+    "uk.gov.hmrc"                %% "domain-play-30"            % "13.0.0",
     "com.github.java-json-tools" %% "json-schema-validator"     % "2.2.14" cross CrossVersion.for3Use2_13,
   )
 
